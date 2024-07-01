a1235dd0fb2d0c428c1238b6b8e2d407f4f44c1d
steven <steven@dstarter.io>
2024-07-01 11:32:25 +0700

    init

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/hello")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Spring Boot!";
+    }
+
+}
